12b0e3e5ce4776a9e6d15c5a8a150971dad4aa65
EdwinIZ8 <37068689+EdwinIZ8@users.noreply.github.com>
2019-12-23 20:30:21 +0800

    Update 数据类型.h
    原以为工作量不小。现在看来，只需要翻译关键字和标点符号就行了。

--- 数据类型.h
+++ 数据类型.h
@@ -18,12 +18,15 @@
   (改为（
   )改为）
   ,改为，
+  
+  *作者介绍
+   张家馨，涿鹿县界牌梁人，生于一九九二年。
 */
 
 #pragma once
 
 /*+----------------------------------------------------------------------------------------+*/
-/*|   					第一部分 新的类型			                   |*/
+/*|   					第一部分 新的类型		                   |*/
 /*+----------------------------------------------------------------------------------------+*/
 /*
 				|基本整数(int)
@@ -49,22 +52,18 @@
 			图1 C语言允许使用的类型（图中有*的是C99所增加的）
 */
 
-typedef int			基本整数;
+typedef int			整数;
 typedef short int		短整数;
 typedef long int		长整数;
 typedef long long int		双长整数;
 typedef	char			字符;
-typedef bool			布尔数;
+typedef bool			布尔;
 typedef float			短小数;
 typedef double			长小数;
 typedef float_complex		短复数小数;
 typedef double_comple		长复数小数;
 typedef long long_comple	双长复数小数;
-#define 枚举类型 enum;
 typedef void 			空类型;
-#define 指针 *;
-#define 结构体 struct;
-#define 共同体 union;
 
 typedef struct
 {
@@ -85,7 +84,7 @@ typedef struct
 typedef (const char) 文件名;
 
 /*+----------------------------------------------------------------------------------------+*/
-/*|					第二部分 替换的字符串				   |*/
+/*|					第二部分 替换的文字     				   |*/
 /*+----------------------------------------------------------------------------------------+*/
 /*
                 auto            enum            restrict        unsigned
@@ -99,33 +98,67 @@ typedef (const char) 文件名;
 		double          long            typedef         
 		else            register        union
 		
-		
-*/
-#define 引用		include;
-#define 规定		define;
-#define 如果规定          ifdef;
-#define 如果没有规定       ifndef;
-#define 取消规定           undef;
-#define 如果              if;
-#define 不然              elif;
-#define 否则              else;
-#define 结束              endif;
-#define 出错              error;
-#define 编译一次          (pragma once);
-#define 编译消息          (pragma message);
-#define 编译警告          (pragma warning);
-#define 编译注释          (pragma comment);
-#define 编译结束          (pragma hdrstop);
-#define 编译资源          (pragma resource);
+		                  图2 C语言的关键字（采用C99标准）
 
-#define 主函数		main;
-#define 标准输入输出	stdio;
-#define 输出		printf;
-#define	输入		scanf;
-#define 正确		1;
-#define 错误		0;
-#define 成功		1;
-#define 失败		0;
+  其中，double、extern、float、inline、long、restrict、short、signed、static、unsigned、volatile、_Bool、_Complex、_Imaginary因为在第一部分已有定义，或用得少，没有替换成自己习惯的文字。
+*/
+#define 引用		include
+#define 规定		define
+#define 如果规定          ifdef
+#define 如果没有规定       ifndef
+#define 取消规定          undef
+#define 如果              if
+#define 不然              elif
+#define 否则              else
+#define 结束              endif
+#define 出错              error
+#define 编译一次          (pragma once)
+#define 编译消息          (pragma message)
+#define 编译警告          (pragma warning)
+#define 编译注释          (pragma comment)
+#define 编译结束          (pragma hdrstop)
+#define 编译资源          (pragma resource)
+
+#define 自动              auto
+#define 中止              break
+#define 情形              case
+#define 固定              const
+#define 继续              continue
+#define 默认              default
+#define 执行              do
+#ifndef 否则              
+#define 否则              else
+#endif
+#define 列举              enum
+//#define extern     
+#define 对于              for
+#define 跳转              goto
+#ifndef 如果
+#define 如果              if
+#endif
+//#define inline
+#define 登记              register
+//#define restrict
+#define 等于              return
+//#define signed
+#define 尺寸              sizeof
+//#define static
+#define 结构              struct
+#define 切换              switch
+#define 别名              typedef
+#define 联合              union
+//#define volatile
+#define 正当              while
+//#define _Bool
+//#define _Complex
+//#define _Imaginary
+
+#define 主函数		main
+
+#define 正确		1
+#define 错误		0
+#define 成功		1
+#define 失败		0
 
 /*+----------------------------------------------------------------------------------------+*/
 /*|                                     第三部分 新的操作                                   |*/
@@ -136,6 +169,7 @@ typedef (const char) 文件名;
 /*+----------------------------------------------------------------------------------------+*/
 /*|                                     第四部分 标点符号                                   |*/
 /*+----------------------------------------------------------------------------------------+*/
+#define 指针 *
 #define , ，
 #define ; 。
 #define ( （
